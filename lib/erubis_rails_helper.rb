f73f959a3b1978a56093b15b31e653f750f8b394
Didip Kerabat <didip@didip-osx.local>
2010-12-30 13:17:51 -0800

    Streamlined the import process.

--- lib/erubis_rails_helper.rb
+++ lib/erubis_rails_helper.rb
@@ -5,16 +5,13 @@ end
 begin
   require 'erubis'
   require 'erubis/preprocessing'
-rescue LoadError
+  require 'erubis/helpers/rails_helper'
+
+  require 'erubis_rails_helper/erubis/generator'
+  require 'erubis_rails_helper/template_handlers/erubis'
+
+rescue LoadError => e
   $stderr.puts %[This erubis_rails requires erubis 2.6.4\n
                  Add "config.gem 'erubis', :version => 2.6.4" to config/environment.rb]
-end  
-
-require 'erubis_rails_helper/erubis/generator'
-require 'erubis_rails_helper/template_handlers/erubis'
-
-## finish
-#if defined?(Rails) and defined?(ActionController)
-#  ActionController::Base.new.logger.info "** Erubis #{::Erubis::VERSION}"
-#end
-#$stdout.puts "** Erubis #{::Erubis::VERSION}"
+  raise e
+end
